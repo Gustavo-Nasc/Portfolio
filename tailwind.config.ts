59958d431bc07a380ebef52994f076720a323db8
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2023-11-07 17:47:55 -0300

    :bookmark: 0.2.0 - :wrench: Configuração adicionada para o TailwindCSS; :fire: Arquivos não utilizados removidos; :bento: Favicon atualizado

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,68 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: {
+        100: '#CCD5FF',
+        400: '#3355ff',
+      }
     },
+    extend: {
+      fontSize: {
+        'heading-lg': ['4rem', {
+          lineHeight: '1.1',
+          fontWeight: '700'
+        }],
+        'heading-md': ['3rem', {
+          lineHeight: '1.1',
+          fontWeight: '700'
+        }],
+        'heading-sm': ['2.25rem', {
+          lineHeight: '1.1',
+          fontWeight: '700'
+        }],
+        'body-xl-bold': ['1.25rem', {
+          lineHeight: '1.5',
+          fontWeight: '700'
+        }],
+        'body-xl-regular': ['1.25rem', {
+          lineHeight: '1.5',
+          fontWeight: '400'
+        }],
+        'body-lg-bold': ['1.125rem', {
+          lineHeight: '1.5',
+          fontWeight: '700'
+        }],
+        'body-lg-regular': ['1.125rem', {
+          lineHeight: '1.5',
+          fontWeight: '400'
+        }],
+        'body-md-bold': ['1rem', {
+          lineHeight: '1.5',
+          fontWeight: '700'
+        }],
+        'body-md-regular': ['1rem', {
+          lineHeight: '1.5',
+          fontWeight: '400'
+        }],
+        'body-sm-bold': ['0.875rem', {
+          lineHeight: '1.5',
+          fontWeight: '700'
+        }],
+        'body-sm-regular': ['0.875rem', {
+          lineHeight: '1.5',
+          fontWeight: '400'
+        }],
+        'body-xs-bold': ['0.75rem', {
+          lineHeight: '1.5',
+          fontWeight: '700'
+        }],
+        'body-xs-regular': ['0.75rem', {
+          lineHeight: '1.5',
+          fontWeight: '400'
+        }],
+      }
+    }
   },
   plugins: [],
 }
